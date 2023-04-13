802a39cceef0a32634611c07417655a59d690f3f
Dtheran <dtheran1@gmail.com>
2023-04-12 22:03:07 -0500

    save data localStorage

--- 3. Junior/social-media-dashboard-with-theme-switcher-master/design/social-media-dashboard-app/src/app/components/header/header.component.ts
+++ 3. Junior/social-media-dashboard-with-theme-switcher-master/design/social-media-dashboard-app/src/app/components/header/header.component.ts
@@ -8,12 +8,40 @@ import { Component, Inject } from '@angular/core';
 })
 export class HeaderComponent {
   isDarkActive = false
+  themeUser = ''
 
-  onChange(newValue: boolean): void {
-    if(newValue) {
-      document.documentElement.classList.add('dark')
+  constructor(@Inject(DOCUMENT)
+    private document: Document,
+  ) {
+    this.getDataLocal()
+    this.isDarkActive = this.document.documentElement.classList.contains('dark');
+  }
+
+  getDataLocal() {
+    this.themeUser = localStorage.getItem('theme') ?? ''
+
+    if (this.themeUser === 'dark') {
+      this.isDarkActive = true
+      this.document.documentElement.classList.toggle('dark')
+    }
+  }
+
+  saveLocal() {
+    this.isDarkActive ? this.themeUser = 'dark' : this.themeUser = 'ligth'
+    localStorage.setItem('theme', (this.themeUser));
+  }
+
+
+  toggleTheme(): void {
+    this.isDarkActive = !this.isDarkActive;
+
+    if (this.isDarkActive) {
+      this.document.documentElement.classList.add('dark')
+      this.saveLocal()
     } else {
-      document.documentElement.classList.remove('dark')
+      this.document.documentElement.classList.remove('dark')
+      this.saveLocal()
     }
   }
 }
+
